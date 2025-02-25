ee615f3e0717aa4323982e45dd181e42ef710bd7
dmitrii.korobov <@Moneyowner>
2025-02-25 11:53:16 +0300

    Классы, подклассы(наследжование, полиморфизм). Методы в классах

--- task1.py
+++ task1.py
@@ -9,3 +9,37 @@
 
 # Метод get_employee_info, который возвращает информацию о сотруднике в
 # виде строки.
+
+class Employee:
+    def __init__(self, name: str, position: str, salary: int):
+        self.__name = name
+        self.__position = position
+        self.__salary = salary
+
+    def set_name(self, new_name: str):
+        self.__name = new_name
+        return f"Имя сотрудника изменено: {new_name}"
+
+    def set_position(self, new_position: str):
+        self.__position = new_position
+
+    def set_salary(self, new_salary: int, new_position: str):
+        if self.__position == new_position:
+            return f"Невозможно повысить ЗП без повышения должности"
+        elif self.__salary >= new_salary:
+            return  f"Невозможно повысить в должности без повышения ЗП"
+        else:
+            self.__salary = new_salary
+            self.__position = new_position
+            return f"Зарплата повышена {new_salary}"
+
+    def get_employee_info(self):
+        return f"name: {self.__name}, position: {self.__position}, salary: {self.__salary}"
+
+
+if __name__ == "__main__":
+    worker = Employee("Dmitry", "QA", 80000)
+
+    print(worker.set_name("Dmitrii"))
+    print(worker.set_salary(100000, "Middle QA"))
+    print(worker.get_employee_info())
--- task3.py
+++ task3.py
@@ -18,3 +18,75 @@
 
 # Метод add_device(device), который добавляет устройство в магазин.
 # Метод list_devices(), который выводит информацию обо всех устройствах.
+
+class Device:
+    def __init__(self, brand: str, model: str, price: int):
+        self.brand = brand
+        self.model = model
+        self.prise = price
+
+    def get_info(self):
+        return f"brand: {self.brand}, model: {self.model}, price: {self.prise}"
+
+    def __repr__(self):
+        return f"brand: {self.brand}, model: {self.model}, price: {self.prise}"
+
+
+class Laptop(Device):
+    def __init__(self, brand: str, model: str, price: int, ram: str, storage: str):
+        super().__init__(brand, model, price)
+        self.ram = ram
+        self.storage = storage
+
+    def get_info(self):
+        return f"{super().get_info()}, ram: {self.ram}, storage: {self.storage}"
+
+    def __repr__(self):
+        return f"{super().__repr__()}, ram: {self.ram}, storage: {self.storage}"
+
+
+class Smartphone(Device):
+    def __init__(self, brand: str, model: str, price: int, camera_megapixels: str, battery_capacity: str):
+        super().__init__(brand, model, price)
+        self.camera_megapixels = camera_megapixels
+        self.battery_capacity = battery_capacity
+
+    def get_info(self):
+        return f"{super().get_info()}, camera_megapixels: {self.camera_megapixels}, battery_capacity: {self.battery_capacity}"
+
+    def __repr__(self):
+        return f"{super().__repr__()}, camera_megapixels: {self.camera_megapixels}, battery_capacity: {self.battery_capacity}"
+
+
+class Store:
+    def __init__(self, devices=None):
+        if devices is None:
+            devices = []
+        self.__devices = devices
+
+    def add_devices(self, device):
+        if not isinstance(device, Device):
+            raise ValueError
+        self.__devices.append(device)
+
+    def list_devices(self):
+        list_of_devices = []
+        for i in self.__devices:
+            list_of_devices.append(i)
+        return list_of_devices
+
+
+if __name__ == "__main__":
+    device1 = Device("JBL", "Headphones", 3000)
+    laptop1 = Laptop("ACER", "NITRO-5", 55000, "Kingston Fury DDR4 8 GB", "SSD 512 GB")
+    smartphone1 = Smartphone("Xiaomi", "Redmi 6", 15000, "30 Mgpx", "3000 mA/h")
+
+    device_list = []
+    store = Store(device_list)
+
+    store.add_devices(smartphone1)
+    store.add_devices(laptop1)
+    store.add_devices(device1)
+
+    print(store.list_devices())
+
